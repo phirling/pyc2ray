d97bb6cfab9484461e5d2f9535966049e1bf179f
phirling <patrick.hirling@epfl.ch>
2023-05-06 14:37:48 +0200

    Restructure part 4 + cleanup

--- old_stuff/misc.py
+++
@@ -1,6 +0,0 @@
-def set_timesteps(cosmo,zred0,zred_end,num_ts,num_out):
-    t1 = cosmo.lookback_time(zred0)
-    t2 = cosmo.lookback_time(zred_end)
-    dt = (t2-t1)/num_ts
-    dt_output = (t2-t1)/num_out
-    return dt, dt_output
--- old_stuff/octa/pure_cpp_version/make_local.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-nvcc -Xcompiler -fPIC -O2 -shared -o RTC.so -I/usr/include/python3.10 -I/home/phirling/miniconda3/envs/astro/lib/python3.10/site-packages/numpy/core/include rtmodule.cpp raytracing.cpp raytracing_gpu.cu -v
-cp RTC.so ../testing_area
-
