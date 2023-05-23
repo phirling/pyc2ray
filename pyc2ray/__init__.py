ed13b60ff8e0d7be1cbe9c78b963f3a430988cc4
phirling <patrick.hirling@epfl.ch>
2023-05-23 12:12:31 +0200

    Changed import order to avoid bug GLIBCXX on Piz Daint

--- pyc2ray/__init__.py
+++ pyc2ray/__init__.py
@@ -1,6 +1,6 @@
+from .objects import C2Ray, C2Ray_test
 from .evolve import *               # Full evolve subroutine: raytracing & chemistry
 from .octa_core import *            # Import and Initialization functions for the OCTA library
 from .raytracing import *           # Raytracing subroutines only
 from .chemistry import *            # Chemistry subroutines only (not yet implemented)
 from .utils import *                # Utility methods: read source files, parameters, write log files, ...
-from .objects import C2Ray, C2Ray_test
